e1a7cdea07227ee1137225136913375b84bc1617
matrixsparse <sparsematrix@163.com>
2017-11-02 17:27:32 +0800

    flask blueprint init

--- flask-celery-simple-demo/flask-blueprint/data/__init__.py
+++ flask-celery-simple-demo/flask-blueprint/data/__init__.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Copyright (C), 2017, matrix
+
 from flask import Blueprint
 
-user = Blueprint('user', __name__,
+data = Blueprint('data', __name__,
                  # template_folder='/opt/auras/templates/',   #指定模板路径
                  # static_folder='/opt/auras/flask_bootstrap/static/',#指定静态文件路径
                  )
 
 
-from user import views
+from data import views
--- flask-celery-simple-demo/flask-blueprint/run.py
+++ flask-celery-simple-demo/flask-blueprint/run.py
@@ -1,19 +1,56 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Copyright (C), 2017, matrix
+
+import re
+import time
+import logging
 from flask import Flask
-from asset import asset
-from user import user
+from data import data
+from spider import spider
+from flask import render_template
+from logging.handlers import TimedRotatingFileHandler
 
-app = Flask(__name__, template_folder='templates',  # 指定模板路径，可以是相对路径，也可以是绝对路径。
-            static_folder='static'  # 指定静态文件前缀，默认静态文件路径同前缀
-            )
 
-app.register_blueprint(asset, url_prefix='/asset')  # 注册asset蓝图，并指定前缀
-app.register_blueprint(user, url_prefix='/user')  # 注册asset蓝图，并指定前缀
+def create_app():
+    app = Flask(__name__,
+                template_folder='templates',  # 指定模板路径，可以是相对路径，也可以是绝对路径
+                static_folder='static'  # 指定静态文件前缀，默认静态文件路径同前缀
+                )
+
+    app.register_blueprint(spider, url_prefix='/spider')  # 注册spider蓝图，并指定前缀
+    app.register_blueprint(data, url_prefix='/data')  # 注册data蓝图，并指定前缀
+
+    lOG_PATH = 'logs/%s' % 'service_'
+    filehandler = TimedRotatingFileHandler(
+        filename=lOG_PATH,
+        when="D",
+        interval=1,
+        # 保留日志个数,默认的0是不会自动删除掉日志
+        backupCount=0
+    )
+
+    datefmt_str = '%Y-%m-%d %H:%M:%S'
+    format_str = '%(asctime)s %(levelname)s %(module)s.%(funcName)s Line:%(lineno)d %(message)s'
+    formatter = logging.Formatter(format_str, datefmt_str)
+    # 设置后缀名称
+    filehandler.suffix = "%Y-%m-%d_%H-%M.log"
+    filehandler.setFormatter(formatter)
+    filehandler.setLevel(logging.DEBUG)
+    app.logger.addHandler(filehandler)
+
+    return app
+
+
+app = create_app()
 
 
 @app.route('/')
 def hello_world():
-    return "<h1>Hello World！</h1>"
+    # return "<h1>Hello World！</h1>"
+    app.logger.debug('index.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)  # 运行flask http程序，host指定监听IP，port指定监听端口，调试时需要开启debug模式。
+    app.run(host='0.0.0.0', port=8000, debug=True)  # 运行flask http程序，host指定监听IP，port指定监听端口，调试时需要开启debug模式
