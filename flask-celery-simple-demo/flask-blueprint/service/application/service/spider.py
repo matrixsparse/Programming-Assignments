d5da4576baefa05e3a34540250e722de2361a252
matrixsparse <sparsematrix@163.com>
2017-11-16 15:02:13 +0800

    update demo code

--- flask-celery-simple-demo/flask-blueprint/service/application/service/spider.py
+++ flask-celery-simple-demo/flask-blueprint/service/application/service/spider.py
@@ -3,41 +3,102 @@
 # @Copyright (C), 2017, matrix
 
 import time
-# 为了操作MongoDB数据库，需要引入pymongo库
 import pymongo
 import requests
 from bs4 import BeautifulSoup
 
+url_list = ['http://www.qu.la/book/3952/']
+
+# url_list = ['http://www.qu.la/book/3952/', 'http://www.qu.la/book/16431/', 'http://www.qu.la/book/3137/']
+
+url_addr = 'http://www.qu.la'
+
 
 class Spider(object):
     def __init__(self):
-        # 连接MongoDB，数据库地址位：localhost，端口号为：27017
-        client = pymongo.MongoClient('localhost', 27017)
-        # 从MongoDB中选择名称为biquge的数据库
-        biquge = client['biquge']
-        info = biquge['info']
+        """
+        配置初始化
+        """
+        self.client = pymongo.MongoClient('localhost', 27017)
+        self.spider = self.client['spider']
+        self.novel = self.spider['novel']
 
-    # 写解析列表页面的代码，先requests库获取网页数据，在BeatifulSoup解析网页数据
-    # 1.把信息插入数据库
-    def insert_info(self, url):
-        # 请求列表页面，获取页面数据
+    def get_chapter_links_from(self, url):
+        """
+        请求列表页面，获取章节页面地址
+        :param url: 
+        :return: 
+        """
+
+        info_list = []
+
         wb_data = requests.get(url)
 
         # print(wb_data.text)
 
-        # 开始解析数据，采用lxml解析引擎
-        # 延迟一秒钟，
         time.sleep(1.5)
-
-        # 开始解析网页数据
         soup = BeautifulSoup(wb_data.text, 'lxml')
-
-        # soup.select 得到的列表，变量名最好是复数形式
         titles = soup.select('#info > h1')
         for i in titles:
-            print('title：', i.get_text())
+            title = i.get_text()
+        chapter_list = soup.select('#list > dl > dd')
+        for i in chapter_list:
+            url = '%s%s' % (url_addr, i.a['href'])
+            chapter = i.get_text()
+            data = {
+                "url": url,
+                "title": title,
+                "chapter": chapter
+            }
+            print(data)
+            info_list.append(data)
+        self.get_content_from(info_list)
+
+    def get_content_from(self, info_list):
+        """
+        获取内容页面内容
+        :param url: 
+        :return: 
+        """
+
+        novel_list = []
+
+        for info in info_list:
+            url = info.get('url', '')
+            if url:
+                wb_data = requests.get(url)
+
+                time.sleep(1.5)
+                soup = BeautifulSoup(wb_data.text, 'lxml')
+                content = soup.select('#content')
+                content = [i.get_text().replace('\xa0', '').replace('\u3000', '') for i in content]
+
+                info['content'] = content
+
+                # self.novel.insert_one(info)
+                novel_list.append(info)
+
+        self.novel.insert_many(novel_list)
+
+    def handle(self):
+        """
+        逻辑处理
+        :return: 
+        """
+        for i in url_list:
+            self.get_chapter_links_from(i)
+
+    def get_data(self):
+        """
+        获取数据
+        :return: 
+        """
+        print('chapter_count:', self.novel.count())
+        # novel_list = self.novel.find()
+        # for i in novel_list:
+        #     print(i)
 
 
 if __name__ == "__main__":
     s = Spider()
-    s.insert_info('http://www.qu.la/book/16431/')
+    s.get_data()
